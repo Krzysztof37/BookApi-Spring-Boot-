a7a489d6924b4621bd167738cc8dfa1a1a3ea600
Krzysztof Jez <krzysztofjez37@tlen.pl>
2021-09-25 15:36:00 +0200

    wyk

--- src/main/java/pl/coderslab/bookapi/entity/Book.java
+++ src/main/java/pl/coderslab/bookapi/entity/Book.java
@@ -18,19 +18,19 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Size(max = 100, message = "isbn: zbyt duzy/maly rozmiar")
+    @Size(max = 100, message = "isbn: za duzo/malo znakow")
     @NotBlank(message = "isbn: wypelnij pole")
     private String isbn;
-    @Size(min = 2,max = 70, message = "title: zbyt duzy/maly rozmiar")
+    @Size(min = 2,max = 70, message = "title: za duzo/malo znakow")
     @NotBlank (message = "title: wypelnij pole")
     private String title;
-    @Size(min=2, max = 30, message = "author: zbyt duzy/maly rozmiar")
+    @Size(min=2, max = 30, message = "author: za duzo/malo znakow")
     @NotBlank(message = "author: wypelnij pole")
     private String author;
-    @Size(min=2,max=50, message = "publisher: zbyt duzy/maly rozmiar")
+    @Size(min=2,max=50, message = "publisher: za duzo/malo znakow")
     @NotBlank(message = "publisher: wypelnij pole")
     private String publisher;
-    @Size(max=40, message = "type: zbyt duzy/maly rozmiar")
+    @Size(max=40, message = "type: za duzo/malo znakow")
     private String type;
 
 
